ac158d070368e13d73f3a03e5e07892d1e10e14e
kavidu717 <dushmanthakavidu143@gmail.com>
2025-12-13 10:18:05 +0530

    setup the react app

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Hello from './components/Hello'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <Hello></Hello>
+     <Hello></Hello>
+    </>
+  )
+}
+
+export default App
